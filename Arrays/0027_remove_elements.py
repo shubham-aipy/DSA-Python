de22102c0e9269dd5f937567fa0f142e5b9c201c
Shubham Sharma <shubhamaiworkspace@gmail.com>
2026-07-28 13:04:46 +0530

    Add Leetcode #27 - Remove Elements

---
+++ Arrays/0027_remove_elements.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in nums:
+            if i != val:
+                nums[k] = i
+                k += 1
+        return k

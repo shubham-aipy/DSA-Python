fa2d9c0bcdba97395e1e737d78246a42c3f1d618
Shubham Sharma <shubhamaiworkspace@gmail.com>
2026-08-01 14:18:17 +0530

    Add Leetcode #844 - Backspace String Compare

---
+++ Arrays/0844_backspace_string_compare.py
@@ -0,0 +1,22 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+
+        stack1 = []
+
+        for char in s:
+            if char == '#':
+                if stack1 != []:
+                    stack1.pop()
+            else:
+                stack1.append(char)
+
+        stack2 = []
+
+        for char in t:
+            if char == '#':
+                if stack2 != []:
+                    stack2.pop()
+            else:
+                stack2.append(char)
+
+        return stack1 == stack2

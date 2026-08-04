87b6975f2e2b3a73b214aa0cdd6e9cdceb48a4e6
Shubham Sharma <shubhamaiworkspace@gmail.com>
2026-08-04 20:44:13 +0530

    Add Leetcode #268 - Missing Number

---
+++ Arrays/0268_missing_number.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        all_nums = set(nums)
+
+        for i in range(n+1):
+            if i not in all_nums:
+                return i
